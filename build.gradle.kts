c4b3c309b414f30477a1684d9a96ee45ffc8ac1f
Petra Puljić <ppuljicc@gmail.com>
2025-06-16 15:50:44 +0200

    Details

--- build.gradle.kts
+++ build.gradle.kts
@@ -16,19 +16,13 @@ repositories {
     google()
 }
 
-// AŽURIRANO: Dodane su ovisnosti za Material Design komponente i ikone
 dependencies {
-    // Osnovna ovisnost za Compose Desktop
     implementation(compose.desktop.currentOs)
 
-    // Ovisnost za Kotlinx Serialization (za rad s JSON-om)
     implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.6.0")
 
-    // NOVO: Ovisnost za Material Design komponente (Button, Card, Scaffold, itd.)
-    // Vjerojatno je ovo uzrok mnogih problema, ne samo ikona.
     implementation(compose.material)
 
-    // NOVO: Ovisnost za prošireni set Material ikona (Forum, People, itd.)
     implementation(compose.materialIconsExtended)
 }
 
